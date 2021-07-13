d3c91922e44828db7f504b794d89655e5eb4228f
Zan Loy <zan.loy@gmail.com>
2021-07-13 14:26:54 -0400

    updated for non-prod environments

--- models/namespace.go
+++ models/namespace.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/informers"
 )
 
 type Namespace struct {
@@ -12,17 +13,27 @@ type Namespace struct {
 	Env     string        `json:"env,omitempty"`
 	Healthy HealthyStatus `json:"healthy"`
 	Errors  []string      `json:"errors,omitempty"`
-	Report  *HealthReport `json:"report,omitempty"`
+
+	Deployments []Deployment `json:"deployments"`
 }
 
 // Takes in a corev1.Namespace from k8 and builds a Namespace.
-func FromK8Namespace(input *corev1.Namespace) Namespace {
+func FromK8Namespace(input *corev1.Namespace, factory informers.SharedInformerFactory) Namespace {
 	tenant, env := parseTenantAndEnv(input.Name)
+	report, err := HealthReportFor(input, factory)
+	if err != nil {
+		report = HealthReport{
+			Healthy: StatusUnknown,
+			Errors:  []string{err.Error()},
+		}
+	}
+
 	ns := Namespace{
 		Name:    input.Name,
 		Tenant:  tenant,
 		Env:     env,
-		Healthy: StatusUnknown,
+		Healthy: report.Healthy,
+		Errors:  report.Errors,
 	}
 
 	// TODO: Get bms configmap
@@ -39,7 +50,7 @@ func parseTenantAndEnv(name string) (string, string) {
 	if strings.Contains(name, "-") {
 		parts := strings.Split(name, "-")
 		switch last := parts[len(parts)-1]; last {
-		case "cola", "perf", "preprod", "prod", "prodtest":
+		case "cola", "demo", "dev", "int", "ivv", "pat", "pdt", "perf", "preprod", "prod", "prodtest", "sqa", "test", "uat":
 			env = last
 			tenant = strings.Join(parts[:len(parts)-1], "-")
 		}
